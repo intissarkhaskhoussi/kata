b02cfbfc7be5cad1ca66ef1bda15097e90b21a81
MoonRabbit <dupiregael@yahoo.fr>
2017-10-28 00:34:15 +0200

    last refactoring : pushing the list into list of NumberDecode object.

--- RomanNumber/Scala/src/main/scala/RomanNumber/NumberDecoder.scala
+++ RomanNumber/Scala/src/main/scala/RomanNumber/NumberDecoder.scala
@@ -8,28 +8,25 @@ class NumberDecoder
   def Decode(number:Int) : String =
   {
     @tailrec
-    def recDecode(it:Int, partialRoman:String) : (String,Int)=
+    def recDecode(number:Int,Value:String, list:List[(String,Int)]) : String=
     {
-      if (it == 0)
-      {
-        return (partialRoman,0)
+      val newList = list.dropWhile(group => group._2 > number)
+      if (newList.isEmpty)
+        return Value
+      newList.head match {
+        case _@(str, i) => recDecode (number - i, Value+ str, newList)
       }
-      val (str,i) = ConversionData.findFirstInferior(it)
-      recDecode(it-i, partialRoman+str)
     }
-    recDecode(number,"")._1
+
+    recDecode(number,"",NumberDecoder.UnitValue)
   }
-}
-
-object ConversionData{
-  val UnitValue = List(("M", 1000), ("CM", 900), ("D", 500), ("CD", 400), ("C", 100),
-    ("XC", 90), ("L", 50), ("XL", 40), ("X", 10),
-    ("IX", 9), ("V", 5), ("IV", 4), ("I",1))
-
-  def findFirstInferior(number:Int) : (String,Int) = UnitValue.filter(e => e._2 <=number).head
 }
 
 object NumberDecoder
 {
   def apply(): NumberDecoder = new NumberDecoder()
+
+  private val UnitValue = List( ("M", 1000), ("CM", 900), ("D", 500), ("CD", 400), ("C", 100),
+    ("XC", 90), ("L", 50), ("XL", 40), ("X", 10),
+    ("IX", 9), ("V", 5), ("IV", 4), ("I",1))
 }
