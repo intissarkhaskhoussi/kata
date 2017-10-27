89b63150cd3537062df7293865618076465de3c9
MoonRabbit <dupiregael@yahoo.fr>
2017-10-28 00:27:17 +0200

    correct all the test by simply fill the list. Is that finished ? No last refactoring ;)

--- RomanNumber/Scala/src/main/scala/RomanNumber/NumberDecoder.scala
+++ RomanNumber/Scala/src/main/scala/RomanNumber/NumberDecoder.scala
@@ -22,7 +22,9 @@ class NumberDecoder
 }
 
 object ConversionData{
-  val UnitValue = List(("L", 50), ("XL", 40), ("X", 10), ("IX", 9), ("V", 5), ("IV", 4), ("I",1))
+  val UnitValue = List(("M", 1000), ("CM", 900), ("D", 500), ("CD", 400), ("C", 100),
+    ("XC", 90), ("L", 50), ("XL", 40), ("X", 10),
+    ("IX", 9), ("V", 5), ("IV", 4), ("I",1))
 
   def findFirstInferior(number:Int) : (String,Int) = UnitValue.filter(e => e._2 <=number).head
 }
