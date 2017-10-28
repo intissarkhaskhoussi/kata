2eec6904c8fbc90aef7d0f1975c9fd19ebc9cf99
MoonRabbit <dupiregael@yahoo.fr>
2017-10-29 00:04:50 +0200

    add Gilded Rose kata and gitignore

---
+++ Gilded_roses/CSharp/GildedRose/GildedRoseTest/ApprovalTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using NUnit.Framework;
+using ApprovalTests;
+using ApprovalTests.Reporters;
+using GildedRoseBusiness;
+
+namespace GildedRoseTests
+{
+	[TestFixture]
+	[UseReporter(typeof(NUnitReporter))]
+	public class ApprovalTest
+	{
+		[Test]
+		public void ThirtyDays()
+		{
+			StringBuilder fakeoutput = new StringBuilder();
+			Console.SetOut(new StringWriter(fakeoutput));
+			Console.SetIn(new StringReader("a\n"));
+
+			Program.Main(new string[] { });
+			String output = fakeoutput.ToString();
+			Approvals.Verify(output);
+		}
+	}
+	
+}
